6cf7f65d175705ef4c8a6c9a397b01946accfcd3
Michał Wojciechowski <129738418+WojciechowskiMichal@users.noreply.github.com>
2023-12-02 22:19:53 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*==================== toggle icon navbar ====================*/
+/*==================== Toggle Icon Navbar ====================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-/*==================== scroll sections active link ====================*/
+/*==================== Scroll Sections Active Link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -26,17 +26,17 @@ window.onscroll = () => {
         }
     });
     
-    /*==================== sticky navbar ====================*/
+    /*==================== Sticky Navbar ====================*/
     let header = document.querySelector('header');
 
     navbar.classList.toggle('sticky', window.scrollY > 100);
 
-    /*==================== remove toggle icon and navbar when clicking navbar links (scroll) ====================*/
+    /*==================== Remove Toggle Icon And Navbar When Clicking Navbar Links (scroll) ====================*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
-/*==================== scroll reveal ====================*/
+/*==================== Scroll Reveal ====================*/
 ScrollReveal({
     // reset: true,
     distance: '80px',
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-img, .activity-container, .portfolio-box, .contact 
 ScrollReveal().reveal(' .about-img, .creator-container, .content2, .left', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content, .creator-container-skils, .content, .right', { origin: 'right' });
 
-/*==================== typed js ====================*/
+/*==================== Typed JS ====================*/
 const typed = new Typed('.multiple-text', {
     strings: ['a Student', 'an Engineer', 'a Frontend Developer', 'a DJ'],
     typeSpeed: 100,
@@ -78,56 +78,84 @@ function openActivity(evt, activityName) {
     document.querySelector(".tab-container").style.display = "block";
 }
 
-function updateProgressBar(progress) {
-    const progressBar = document.getElementById("progress-bar");
-    progressBar.style.width = progress + "%";
-    progressBar.innerHTML = progress + "%";
+
+/*==================== Close Notification ====================*/
+function closenotification() {
+    var notification = document.getElementById("notification");
+    notification.style.display = "none";
 }
 
-function zamknijPowiadomienie() {
-    var powiadomienie = document.getElementById("powiadomienie");
-    powiadomienie.style.display = "none";
-}
-
+/*==================== Contact Form ====================*/
 document.addEventListener("DOMContentLoaded", function () {
-    var messageContainer = document.getElementById("message-container");
-    var powiadomienie = document.getElementById("powiadomienie");
-    var messageText = document.getElementById("message-text");
+    var form = document.getElementById("myForm");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    if (messageContainer) {
-        messageContainer.style.display = "none";
-        
-        var form = document.querySelector("form");
-        form.addEventListener("submit", function (event) {
-            event.preventDefault();
-        
-            var formData = new FormData(form);
-        
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "/php/send_form.php", true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        var response = JSON.parse(xhr.responseText);
-                        if (response.success) {
-                            messageText.innerText = response.message;
-                            powiadomienie.style.display = "block";
-        
-                            form.reset();
-                        } else {
-                            // Obsługa błędu
-                            alert("Wystąpił błąd: " + response.message);
-                        }
+        if (!validateForm()) {
+            alert("Please fix the errors in the form.");
+            return;
+        }
+
+        var formData = new FormData(form);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/php/send_form.php", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    if (response.success) {
+                        document.getElementById("message-text").innerText = response.message;
+                        document.getElementById("notification").style.display = "block";
+                        form.reset();
                     } else {
-                        alert("Wystąpił błąd: " + xhr.status);
+                        alert("An error occurred: " + response.message);
                     }
+                } else {
+                    alert("An error occurred: " + xhr.status);
                 }
-            };
-            xhr.send(formData);
-        });
-        
-    }
+            }
+        };
+        xhr.send(formData);
+    });
 });
+
+/*==================== Email Validations ====================*/
+
+function validateForm() {
+    var fullName = document.forms["myForm"]["full_name"].value;
+    var email = document.forms["myForm"]["email"].value;
+    var email_subject = document.forms["myForm"]["email_subject"].value;
+    var mobileNumber = document.forms["myForm"]["mobile_number"].value;
+    var message = document.forms["myForm"]["message"].value;
+
+    if (fullName.length > 35) {
+        alert("Full Name must not exceed 35 characters.");
+        return false;
+    }
+
+    if (email.length > 50) {
+        alert("Email must not exceed 50 characters.");
+        return false;
+    }
+
+    if  (email_subject.length > 100){
+        alert("Email Subject mus not exceed 100 characters.")
+        return false;
+    }
+
+    if (!mobileNumber.match(/^\d{0,9}$/)) {
+        alert("Mobile Number must be up to 9 digits.");
+        return false;
+    }
+
+    if (message.length > 300) {
+        alert("Message must not exceed 300 characters.");
+        return false;
+    }
+
+    return true;
+}
 
 function enableSubmit() {
     document.getElementById('submit-button').disabled = false;
